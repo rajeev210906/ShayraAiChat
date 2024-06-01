b067cfd9d21c18e3ca5bac50722e8d832d839b53
Rajeev Ranjan <168575330+rajeev210906@users.noreply.github.com>
2024-06-02 02:56:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 app = Flask(__name__)
 
 # Set the environment variable for the API key
-os.environ['GOOGLE_API_KEY'] = "AIzaSyDEgnZhtY_RpDQ-bvpPJNOCaLQCceEVCWc"
+os.environ['GOOGLE_API_KEY'] = "YOUR_API_KEY"
 
 # Initialize the ChatGoogleGenerativeAI class
 llm = ChatGoogleGenerativeAI(model="gemini-pro")
